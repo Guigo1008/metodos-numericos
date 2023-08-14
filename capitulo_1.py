b20df1cfa5c0e6af3a87544cbdec72a05ed0632e
Guilherme Xavier <guigo@eq.ufrj.br>
2023-08-13 23:01:20 -0300

    Uploading chapter 1

---
+++ capitulo_1.py
@@ -0,0 +1,44 @@
+import math
+import numpy as np
+
+# Algorítmo 1:
+
+
+def algoritmo_1(N):
+    n = int(math.log2(N)) + 1
+    P = N
+    a = np.array()
+
+    for i in range(n):
+        M = P/2.
+        P = int(M)
+        # Isso é igual a fazer P%2 (que existe em python)
+        a.append(int(2*(M-P)))
+
+    return a
+
+
+def teste_algoritmo_1(lista_bin):
+    n = 0
+    for i in range(len(lista_bin)):
+        n += lista_bin[i] * 2**i
+
+    return n
+
+
+""" lista_binaria = algoritmo_1(10)
+print(lista_binaria)
+print(teste_algoritmo_1(lista_binaria)) """
+
+
+# Algoritmo 2
+def algoritmo_2(N):
+    n = 0
+    pot = 1
+    while pot <= N:
+        n = n + 1
+        pot = 2 * pot
+    n = n - 1
+
+    pot = 2**n
+    M = N - pot
